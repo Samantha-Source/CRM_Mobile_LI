afe616102e231f4d55143fa9d0f296e0741ad64f
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-07-28 20:31:21 -0400

    Add Person Started

--- src/components/AddPerson.js
+++ src/components/AddPerson.js
@@ -1,9 +1,15 @@
 import React, { Component} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Button, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/EvilIcons';
+import { MKTextField, MKColor, MKButton } from 'react-native-material-kit';
 import * as actions from '../actions';
 
+const AddButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.addButtonContainer}>
+        <Text style={styles.addButtonText}>{title}</Text>
+    </TouchableOpacity>
+);
 
 class AddPerson extends Component {
     static navigationOptions = {
@@ -12,19 +18,58 @@ class AddPerson extends Component {
         )
     }
 
+
+
     render() {
         return (
-            <View style={styles.container}>
+            <View style={styles.form}>
                 <Text>Add person screen</Text>
+                <AddButton title="Button" size="sm" backgroundColor="#007bff" />
+                
+                {/* <Button
+                    title="Button"
+                    onPress={() => console.log('pressed button')}
+                    color="#2196F3"
+                    accessibilityLabel='The info about the button'
+                /> */}
+                
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-
+    form: {
+        flex: 1,
+        paddingTop: 50,
+        paddingBottom: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        justifyContent: 'space-between',
     },
+    fieldStyles: {
+        height: 40,
+        color: MKColor.Orange,
+    },
+    addButton: {
+        marginTop: 20,
+    },
+    addButtonContainer: {
+        alignSelf: 'center',
+        backgroundColor: '#ABEDC6',
+        borderRadius: 50,
+        width: 150,
+        height: 50,
+        justifyContent: 'center'
+        
+    },
+    addButtonText: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: 'black',
+    }
+
 })
 
 export default connect(null, actions)(AddPerson);
