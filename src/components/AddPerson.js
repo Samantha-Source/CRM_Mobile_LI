8e32cd16b61fcd681a9846f9363450894904b29c
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-07-31 19:11:18 -0400

    AddPerson Form

--- src/components/AddPerson.js
+++ src/components/AddPerson.js
@@ -1,15 +1,24 @@
 import React, { Component} from 'react';
-import { View, Text, StyleSheet, ScrollView, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Button, TouchableOpacity, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/EvilIcons';
-import { MKTextField, MKColor, MKButton } from 'react-native-material-kit';
+import { MKColor } from 'react-native-material-kit';
 import * as actions from '../actions';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Fumi } from 'react-native-textinput-effects';
 
 const AddButton = ({ onPress, title }) => (
     <TouchableOpacity onPress={onPress} style={styles.addButtonContainer}>
         <Text style={styles.addButtonText}>{title}</Text>
     </TouchableOpacity>
 );
+
+// const TextField = ({title, onChange }) => (
+//     <MKTextField
+//         placeholder={title}
+//         onChangeText={onChange}
+//     />
+// )
 
 class AddPerson extends Component {
     static navigationOptions = {
@@ -19,21 +28,95 @@ class AddPerson extends Component {
     }
 
 
-
     render() {
         return (
-            <View style={styles.form}>
-                <Text>Add person screen</Text>
-                <AddButton title="Button" size="sm" backgroundColor="#007bff" />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.form}>
+                    <Fumi
+                        label="First Name"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'user'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Last Name"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'user'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Phone Number"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'phone'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Email"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'envelope'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Company"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'university'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Project"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'folder-open'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+                    <Fumi
+                        label="Notes"
+                        iconClass={FontAwesomeIcon}
+                        iconName={'pencil-square-o'}
+                        iconColor={MKColor.Amber}
+                        iconSize={20}
+                        iconWidth={40}
+                        inputPadding={16}
+                        style={styles.fieldStyles}
+                    />
+
+
+                    
+                </View>
+
+
+                <View>
+                    <AddButton title="Button" size="sm" backgroundColor="#007bff" onPress={() => console.log("pressed Add Person button")} />
+                </View>
+
+
                 
-                {/* <Button
-                    title="Button"
-                    onPress={() => console.log('pressed button')}
-                    color="#2196F3"
-                    accessibilityLabel='The info about the button'
-                /> */}
                 
-            </View>
+                
+          </ScrollView>
         )
     }
 }
@@ -41,15 +124,17 @@ class AddPerson extends Component {
 const styles = StyleSheet.create({
     form: {
         flex: 1,
-        paddingTop: 50,
+        paddingTop: 10,
         paddingBottom: 10,
         paddingLeft: 20,
         paddingRight: 20,
         justifyContent: 'space-between',
+        // backgroundColor: MKColor.BlueGrey,
     },
     fieldStyles: {
-        height: 40,
-        color: MKColor.Orange,
+        backgroundColor: '#ECFFEB',
+        borderRadius: 20,
+        marginBottom: 10,
     },
     addButton: {
         marginTop: 20,
